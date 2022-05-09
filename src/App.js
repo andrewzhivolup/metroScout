4499dc673b8a39556fac2f4ab7d3dd70b6f811e5
andrewzhivolup <ferari1190@mail.ru>
2022-05-10 00:11:02 +0300

    version 0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useMemo, useState } from 'react';
 import Map from './components/Map';
 
 function App() {
+  const [stations, setStations] = useState([
+    { id: 1, nameStation: 'Текстильщики', coors: [55.7092094, 37.7332191], isDone: false},
+    { id: 2, nameStation: 'Кузьминки', coors: [55.7055473, 37.7662201], isDone: true},
+  ])
+
   return (
     <div className="App">
-      <Map />
+      <Map stations = {stations}/>
     </div>
   );
 }
--- src/components/CustomMarker/CustomMarker.jsx
+++ src/components/CustomMarker/CustomMarker.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import L from "leaflet";
-import CustomMarkerSvg from "../../../src/img/pin-false.svg";
-import { Marker, Popup } from "react-leaflet";
+import { Marker, Popup, SVGOverlay } from "react-leaflet";
 import UploadForm from "../UI/UploadForm/UploadForm";
+import { Switch } from 'antd';
+import 'antd/dist/antd.css';
+
+import CustomMarkerSvgFalse from "../../../src/img/pin-false.svg";
+import CustomMarkerSvgTrue from "../../../src/img/pin-true.svg";
+import metroMapLeb from '../../../src/img/metro-map.svg';
 
 const CustomMarker = (props) => {
   const SwapDefaultPin = new L.Icon({
-    iconUrl: CustomMarkerSvg,
+    iconUrl: (props.marker.isDone ? CustomMarkerSvgTrue : CustomMarkerSvgFalse),
     iconSize: [85, 85],
   });
 
+  function onChange(checked) {
+    console.log(`switch to ${checked}`);
+
+  }
   
-
-  const position = [55.7522, 37.6156];
-
   return (
     <Marker
       id={props.marker.id}
@@ -22,7 +28,11 @@ const CustomMarker = (props) => {
     >
       <Popup>
         {props.marker.nameStation}
+        <br/>
+        <Switch defaultChecked={props.marker.isDone} onChange={onChange} />
+        
       </Popup>
+    
     </Marker>
   );
 };
